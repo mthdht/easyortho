05917efc83186c0d71c557c7c897fc130e6f61e1
mthdht <mthdht@gmail.com>
2024-01-30 10:26:49 +0100

    start homepage (jumbotron)

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,1 +1,11 @@
-<template>OK</template>
+<template>
+    <section class="jumbotron h-60">
+        <div class="container py-20">
+            <h1 class="text-3xl font-bold">titre de la page</h1>
+            <p class="lead mb-4 font-semibold text-slate-500">Une description du site </p>
+            <router-link to="/">
+                <button class="rounded-full px-4 py-2 bg-sky-600 text-white font-semibold">S'inscrire</button>
+            </router-link>
+        </div>
+    </section>
+</template>
