06cf0f6e10b17b5510ff4be4a587e08801d54b43
mthdht <mthdht@gmail.com>
2024-01-31 10:12:46 +0100

    finish style for category section

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -35,16 +35,16 @@
         </div>
     </section>
 
-    <section class="lesson-categories py-20 bg-slate-200">
+    <section class="lesson-categories py-20 bg-slate-100">
         <div class="container">
 
             <h2 class="text-3xl font-bold text-center">titre de la section</h2>
-            <div class="grid grid-cols-6 gap-4 px-20 mt-12 transition-transform">
+            <div class="grid grid-cols-6 gap-4 px-20 mt-12 transition-all">
                 <router-link to="/">    
-                    <div class="category bg-red-200 p-4 aspect-square flex flex-col items-center justify-center hover:bg-red-300 rounded shadow hover:shadow-lg hover:scale-[1.03] transition-transform">
+                    <div class="category group bg-white p-4 aspect-square flex flex-col items-center justify-center hover:bg-cyan-700 rounded shadow hover:shadow-lg hover:scale-[1.03] transition-transform">
                         <div class="icon size-20 bg-red-600 mb-4"></div>
-                        <h3 class="font-semibold">fonctionalité</h3>
-                        <p class="px-2 py-1 rounded bg-slate-100 text-xs font-bold mt-1">15 cours</p>
+                        <h3 class="font-semibold group-hover:text-white text-xl">fonctionalité</h3>
+                        <p class="px-2 py-1 rounded bg-cyan-50 text-cyan-700 text-xs font-bold mt-1">15 cours</p>
                     </div>
                 </router-link>
                 
