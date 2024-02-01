b48126133a9d7310dba33dca74153880ff83a7d2
mthdht <mthdht@gmail.com>
2024-02-01 08:20:01 +0100

    made carousel components

---
+++ src/components/Carousel.vue
@@ -0,0 +1,87 @@
+<template>
+    <section class="flex overflow-hidden gap-4 relative" ref="carousel">
+      <slot class="test"></slot>
+      <div
+        class="
+          prev
+          h-40
+          bg-slate-500/50
+          w-10
+          absolute
+          left-0
+          flex
+          items-center
+          justify-center
+          text-white
+        "
+        v-show="currentItem != 0"
+        @click="prev"
+      >
+        <ChevronLeftIcon></ChevronLeftIcon>
+      </div>
+      <div
+        class="
+          next
+          h-40
+          bg-slate-500/50
+          w-10
+          absolute
+          right-0
+          flex
+          items-center
+          justify-center
+          text-white
+        "
+        v-show="slideLeft != 0"
+        @click="next"
+      >
+        <ChevronRightIcon></ChevronRightIcon>
+      </div>
+    </section>
+  </template>
+  
+  <script setup>
+  import { ChevronRightIcon, ChevronLeftIcon } from '@heroicons/vue/24/outline';
+  import { computed, provide, ref, useSlots } from 'vue';
+  
+  const props = defineProps({
+    color: {
+      type: String,
+      default: 'gray',
+      validator(value) {
+        // The value must match one of these strings
+        return ['gray', 'red', 'orange', 'yellow', 'green', 'blue'].includes(
+          value
+        );
+      },
+    },
+    visibleItems: {
+      type: Number,
+      default: 5,
+    },
+    aspect: {
+      type: String,
+      default: '4/3',
+    },
+  });
+  
+  const currentItem = ref(0);
+  const carousel = ref('carousel');
+  const slots = useSlots().default();
+  const slideLeft = computed(() => {
+    return slots.length - props.visibleItems - currentItem.value;
+  });
+  
+  provide('currentItem', currentItem);
+  provide('slideLeft', slideLeft);
+  provide('aspect', props.aspect);
+  
+  function next() {
+    currentItem.value++;
+  }
+  
+  function prev() {
+    currentItem.value--;
+  }
+  </script>
+  
---
+++ src/components/CarouselItem.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="carousel-item shrink-0 bg-red-500" ref="item">
+      <slot></slot>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ChevronRightIcon, ChevronLeftIcon } from '@heroicons/vue/24/outline';
+  import { computed, inject, getCurrentInstance, ref } from 'vue';
+  
+  const instance = getCurrentInstance();
+  const currentItem = inject('currentItem');
+  const slideLeft = inject('slideLeft');
+  const aspect = inject('aspect');
+  
+  const size = computed(() => {
+    return (
+      (instance.parent.refs.carousel.offsetWidth -
+        instance.parent.props.visibleItems * 16) /
+      (instance.parent.props.visibleItems + 0.5)
+    );
+  });
+  
+  const pixelSize = computed(() => {
+    return size.value + 'px';
+  });
+  
+  const translateSize = computed(() => {
+    if (slideLeft.value == 0) {
+      return '-' + (size.value + 16) * (currentItem.value - 0.5) + 'px';
+    }
+    return '-' + (size.value + 16) * currentItem.value + 'px';
+  });
+  </script>
+  
+  <style>
+  .carousel-item {
+    width: v-bind(pixelSize);
+    transform: translateX(v-bind(translateSize));
+    transition: transform 1s ease;
+    aspect-ratio: v-bind(aspect);
+  }
+  </style>
+  
